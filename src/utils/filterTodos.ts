56dd4dfaf8e0f9b2ac178ee2ec6f00ef1de5193b
Medvid Oleksandr <samaster010604@gmail.com>
2024-07-30 12:03:31 +0300

    Todo with API

---
+++ src/utils/filterTodos.ts
@@ -0,0 +1,13 @@
+import { Todo } from '../types/Todo';
+import { FilterTupes } from '../types/Filters';
+
+export const filterTodos = (todos: Todo[], filter: FilterTupes): Todo[] => {
+  switch (filter) {
+    case FilterTupes.Active:
+      return todos.filter(todo => !todo.completed);
+    case FilterTupes.Completed:
+      return todos.filter(todo => todo.completed);
+    default:
+      return todos;
+  }
+};
